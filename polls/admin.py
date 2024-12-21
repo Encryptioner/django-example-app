262bcd952d03c51148a6c467513efcf5641131c7
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2024-12-21 09:03:46 +0600

    feat: show add choices in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
 
-# Register your models here.
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
