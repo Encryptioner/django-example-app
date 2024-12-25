016a30a59fd4acdf364b22355218efbb90af05f3
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2024-12-25 11:47:10 +0600

    feat: view client user from admin side

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ClientUser
+
+
+class ClientUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "last_login",
+        "date_joined",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = ("date_joined",)
+    search_fields = ("name", "email", "id")
+
+
+admin.site.register(ClientUser, ClientUserAdmin)
